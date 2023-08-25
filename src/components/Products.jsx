25c28fddee8e52da63774081aa01bd955f9c2a30
Vitaly Park <126284852+gitvitaly@users.noreply.github.com>
2023-08-25 12:11:48 -0700

    Update Products.jsx

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,1 +1,31 @@
-import axios from "axios";import {useEffect, useState} from "react";const url = 'https://fakestoreapi.com/products'async function Products() {    const [product, setProduct] = useState([])    useEffect(() => {        async function storeData() {            const response = await axios.get(url)            setProduct(response.data.title)                    }        storeData()    }, [])    return (        <div>            <h2>                {product}            </h2>        </div>    );}export default Products;
+import axios from "axios";
+import {useEffect, useState} from "react";
+
+const url = 'https://fakestoreapi.com/products'
+
+
+async function Products() {
+    const [product, setProduct] = useState([])
+    useEffect(() => {
+        async function storeData() {
+            const response = await axios.get(url)
+            setProduct(response.data.title)
+            
+        }
+
+        storeData()
+
+    }, [])
+
+
+    return (
+        <div>
+            <h2>
+                {product}
+            </h2>
+        </div>
+    );
+}
+
+
+export default Products;
